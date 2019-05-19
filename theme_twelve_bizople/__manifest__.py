eb66ecd8975d47491fea2cc51df1957d918b1488
Arjan Rosman <9072317+AardugNL@users.noreply.github.com>
2019-05-20 01:07:22 +0530

    Add files via upload

---
+++ theme_twelve_bizople/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Theme Twelve',
+    'category': 'Theme',
+    'version': '12.0.0.1',
+    'author': 'Bizople Solutions Pvt. Ltd.',
+    'website': 'http://www.bizople.com',
+    'summary': '''Software, Web Agency, Charity, Landing, Real Estate,Agency, Hotel/Restaurant, Health care / Medical Agency or a Hospital, Accounting Firm, Fitness Centre, SEO, Fashion, Civil Engineer, Furniture / Interior Designing, Service provider, or any other''',
+    
+    'description': """Twelve is an amazing adaptable Odoo Theme on store right now. Weather
+                you're Software Application Developer, Web Agency, Charity
+                Organisation, Creative/ Artistic Person/Organisation, Real Estate
+                Agency, Hotel/Restaurant, Health care / Medical Agency or a
+                Hospital, Accounting Firm, Fitness Centre, SEO Agency, Fashion
+                Agency, Architect/Civil Engineer, Furniture / Interior Designing
+                Agency, Service provider, Businessman or any other, this theme is a
+                perfect fit for your Magic Website!""",
+    'depends': [
+        'website_theme_install',
+        'theme_default',
+    ],
+    'live_test_url': 'https://theme-twelve.bizople.com',
+    'images': [
+        'static/description/twelve_desc.png',
+         'static/description/twelve_screenshot.png'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'OPL-1',
+    'price': 199,
+    'currency': 'EUR',
+}
